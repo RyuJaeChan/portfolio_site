c38ccd4917146b14a06057a43f198ef2c8ba20ae
RyuJaeChan <rjch221@gmail.com>
2018-04-11 03:19:40 +0900

    1
    1

--- route/content.js
+++ route/content.js
@@ -2,7 +2,10 @@
 
 var content_get = function(req, res){
     console.log('>> /content (get)')
-    res.render('contents')
+
+    var data = {text : "<p>안녕하세용ㅇ용ㅇ</p>"}
+
+    res.render('contents', data)
 }
 
 module.exports.content_get = content_get;
